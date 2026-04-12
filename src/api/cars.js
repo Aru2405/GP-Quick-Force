c9039938f49ba139623fa340ac2d7baaec5885c5
Jannafer <jannafer.hb@gmail.com>
2026-04-12 19:05:00 +0200

    cars

--- src/api/cars.js
+++ src/api/cars.js
@@ -1,10 +1,20 @@
-import client from './client'
+import client from './client';
 
+// Obtener coches
 export async function getCars() {
   const resp = await client.get('/cars')
   return resp.data
 }
+// Crear coche
+export const createCar = (carData) => {
+  return client.post("/cars", carData);
+};
 
+// Actualizar coche
+export const updateCar = (id, carData) => {
+  return client.put(`/cars/${id}`, carData);
+};
+// Eliminar coche
 export async function deleteCar(id) {
   await client.delete(`/cars/${id}`)
 }
